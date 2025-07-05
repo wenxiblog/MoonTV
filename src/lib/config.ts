a071eefd8a781d78c3b487c7a733e34047b96ff6
wenxi <nnceng240@gmail.com>
2025-07-05 21:40:19 +0800

    Update config.ts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -150,7 +150,7 @@ async function initConfig() {
           }
           adminConfig = {
             SiteConfig: {
-              SiteName: process.env.SITE_NAME || 'MoonTV',
+              SiteName: process.env.SITE_NAME || 'WenXiTV',
               Announcement:
                 process.env.NEXT_PUBLIC_ANNOUNCEMENT ||
                 '本网站仅提供影视信息搜索服务，所有内容均来自第三方网站。本站不存储任何视频资源，不对任何内容的准确性、合法性、完整性负责。',
@@ -190,7 +190,7 @@ async function initConfig() {
     // 本地存储直接使用文件配置
     cachedConfig = {
       SiteConfig: {
-        SiteName: process.env.SITE_NAME || 'MoonTV',
+        SiteName: process.env.SITE_NAME || 'WenXiTV',
         Announcement:
           process.env.NEXT_PUBLIC_ANNOUNCEMENT ||
           '本网站仅提供影视信息搜索服务，所有内容均来自第三方网站。本站不存储任何视频资源，不对任何内容的准确性、合法性、完整性负责。',
